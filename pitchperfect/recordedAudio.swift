53dd8b960681f2b4a58c222efe5122f1090e9364
Guilherme Carvalho <guilherme@Guilhermes-MacBook-Pro.local>
2015-06-29 09:29:19 -0700

    Model for the Recorded audio passed between view controllers.

---
+++ pitchperfect/recordedAudio.swift
@@ -0,0 +1,14 @@
+//
+//  recordedAudio.swift
+//  pitchperfect
+//
+//  Created by Guilherme Carvalho on 6/28/15.
+//  Copyright (c) 2015 Guilherme Carvalho. All rights reserved.
+//
+
+import Foundation
+
+class RecordedAudio: NSObject{
+    var filePathUrl: NSURL!
+    var title: String!
+}
